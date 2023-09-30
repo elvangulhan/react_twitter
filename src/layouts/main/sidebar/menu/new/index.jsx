a74cebebf3bd492402f461aea1abe039d19bfa33
ELVAN <93934504+elvangulhan@users.noreply.github.com>
2023-09-30 08:35:16 +0300

    part 2

---
+++ src/layouts/main/sidebar/menu/new/index.jsx
@@ -0,0 +1,11 @@
+import Button from "~/components/button";
+
+function New() {
+    return (
+        <div className="py-4 w-[90%]">
+            <Button size="large">Gönder</Button>
+        </div>
+    )
+}
+
+export default New;
